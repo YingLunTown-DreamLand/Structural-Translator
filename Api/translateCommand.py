8689b9ff095b9f0684cda87316df2aa21c5bf36f
Happy2018new <109064184+Happy2018new@users.noreply.github.com>
2022-10-15 00:04:52 +0800

    Update translateCommand.py

--- Api/translateCommand.py
+++ Api/translateCommand.py
@@ -44,7 +44,7 @@ def getRightBarrier(command:str,pointer:int)->int:
 
 def searchForExecute(command:str,pointer:int)->list:
     pointer = jumpSpace(command,pointer)
-    if command[pointer:pointer+7].replace('E','e').replace('X','x').replace('C','c').replace('U','u',).replace('T','t') == 'execute':
+    if command[pointer:pointer+7].replace('E','e').replace('X','x').replace('C','c').replace('U','u',).replace('T','t') == 'execute' or '/execute':
         return [pointer+7,True]
     else:
         return [pointer,False]
@@ -102,7 +102,7 @@ def getPos(command:str,pointer:int)->list:
         if command[pointer] == '^' or command[pointer] == '~':
             transit = highSearching(command,pointer+1,[' ','^','~','a','b','c','d','e','f','g','h','i','j','k','l','m','n',
             'o','p','q','r','s','t','u','v','w','x','y','z','A','B','C','D','E','F','G','H','I','J','K','L','M','N','O',
-            'P','Q','R','S','T','U','V','W','X','Y','Z'])
+            'P','Q','R','S','T','U','V','W','X','Y','Z','/','?'])
             ans.append(
                 command[
                     pointer:
