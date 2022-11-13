ad32cfb102847f5f7c8d52ed9ab93fed25c94280
Happy2018new <chenbo79800@163.com>
2022-11-13 14:17:35 +0800

    fixed an issue related to Android paths

--- Api/ParseBDX/Batch.py
+++ Api/ParseBDX/Batch.py
@@ -14,8 +14,8 @@ outputPath = 'Api/ParseBDX/Output'    # 在哪里输出 JSON 文件
 
 
 
-searchPath = os.path.normpath(searchPath)
-outputPath = os.path.normpath(outputPath)
+searchPath = os.path.normpath(searchPath).replace('\\','/')
+outputPath = os.path.normpath(outputPath).replace('\\','/')
 # 格式化处理
 
 
@@ -25,13 +25,13 @@ outputPath = os.path.normpath(outputPath)
 filePath = []
 for root,dirs,files in os.walk(searchPath):
     for fileName in files:
-        String = os.path.normpath(os.path.normpath(os.path.join(root,fileName))).split('\\')
+        String = os.path.normpath(os.path.join(root,fileName)).replace('\\','/').split('/')
         String[-1] = String[-1].split('.')
         if len(String[-1]) > 0:
             String[-1][-1].replace('B','b').replace('D','d').replace('X','x')
         String[-1] = ".".join(String[-1])
         if os.path.splitext(String[-1])[-1] == '.bdx':
-            filePath.append("\\".join(String))
+            filePath.append("/".join(String))
 print(f'A total of {len(filePath)} files were found.')
 # 找到 searchPath 下的所有 BDX 文件
 
@@ -58,16 +58,16 @@ for i in filePath:
         continue
     # 解析文件
 
-    mkdirPath = os.path.normpath(os.path.join(outputPath,i.replace(searchPath + '\\','',1))).split('\\')
+    mkdirPath = os.path.normpath(os.path.join(outputPath,i.replace(searchPath + '/','',1))).replace('\\','/').split('/')
     del mkdirPath[-1]
-    mkdirPath = "\\".join(mkdirPath)
+    mkdirPath = "/".join(mkdirPath)
     # 确定文件夹创建路径
 
     if not os.path.exists(mkdirPath):
         os.makedirs(mkdirPath)
     # 创建文件夹，若对应的文件夹不存在的话
 
-    outputLocation = os.path.normpath(os.path.join(outputPath,i.replace(searchPath + '\\','',1) + '.json'))
+    outputLocation = os.path.normpath(os.path.join(outputPath,i.replace(searchPath + '/','',1) + '.json')).replace('\\','/')
     with open(outputLocation,"w+",encoding='utf-8') as file:
         file.write(json.dumps(Parse.resultList,sort_keys=True,indent=4,separators=(', ', ': '),ensure_ascii=False))
     # 写入到文件
@@ -86,7 +86,7 @@ if not os.path.exists(outputPath):
     os.makedirs(outputPath)
 # 创建文件夹，若对应的文件夹不存在的话
 
-logOutputLocation = os.path.normpath(os.path.join(outputPath,'ParseBDX_log.json'))
+logOutputLocation = os.path.normpath(os.path.join(outputPath,'ParseBDX_log.json')).replace('\\','/')
 
 with open(logOutputLocation,"w+",encoding='utf-8') as file:
     file.write(json.dumps(logList,sort_keys=True,indent=4,separators=(', ', ': '),ensure_ascii=False))
